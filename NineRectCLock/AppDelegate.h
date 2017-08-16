da482bd4e579188a491b21541386deecd3e07ac1
Dobby <jevo747@yeah.net>
2017-08-16 09:22:34 +0800

    Initial Commit

---
+++ NineRectCLock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NineRectCLock
+//
+//  Created by v_ljiayili(李嘉艺) on 2017/8/16.
+//  Copyright © 2017年 Dobby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
